6f4822ab865cc1903f18f49a4fb56f578d11b184
unknown <youssefamr212@gmail.com>
2025-09-30 12:45:46 +0300

    error solving

--- db/upgrade.php
+++ db/upgrade.php
@@ -16,50 +16,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-function xmldb_learningmap_upgrade($oldversion) {
-    global $DB;
-
-    $dbman = $DB->get_manager();
-
-    if ($oldversion < 2025093001) {
-        $table = new xmldb_table('learningmap');
-        $field = new xmldb_field('automode', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'showmaponcoursepage');
-
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        upgrade_mod_savepoint(true, 2025093001, 'learningmap');
-    }
-
-    return true;
-}
-
-<?php
-// This file is part of Moodle - https://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
-
-/**
- * Upgrade functions for mod_learningmap
- *
- * @package     mod_learningmap
- * @copyright 2021-2024, ISB Bayern
- * @author      Stefan Hanauska <stefan.hanauska@csg-in.de>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 /**
  * Define upgrade steps to be performed to upgrade the plugin from the old version to the current one.
  *
@@ -132,6 +88,18 @@ function xmldb_learningmap_upgrade($oldversion) {
 
         // Learningmap savepoint reached.
         upgrade_mod_savepoint(true, 2024072201, 'learningmap');
+    }
+
+    // Add automode field to learningmap table.
+    if ($oldversion < 2025093001) {
+        $table = new xmldb_table('learningmap');
+        $field = new xmldb_field('automode', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'showmaponcoursepage');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        upgrade_mod_savepoint(true, 2025093001, 'learningmap');
     }
 
     return true;
